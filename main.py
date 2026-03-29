5836fd3d4e24d6bd63953a0774b8a160fb402660
RobertReyes-UEA <rm.reyesn@uea.edu.ec>
2026-03-29 18:01:06 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+
+import tkinter as tk
+from servicios.tarea_servicio import TareaServicio
+from ui.app_tkinter import AppTkinter
+
+def main():
+    root = tk.Tk()
+    servicio = TareaServicio()
+    app = AppTkinter(root, servicio)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
